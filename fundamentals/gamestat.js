2206df07ecbdd8ad0bd303c23186a2420cfbf41d
kimcipher <kimutaij192@gmail.com>
2023-04-19 09:14:12 +0300

    keyboard events

---
+++ fundamentals/gamestat.js
@@ -0,0 +1,25 @@
+// Define "gameState" here
+const gameState = {};
+
+function create() {
+  // Create a circle and assign it to gameState.circle here
+ gameState.circle = this.add.circle(20, 20, 5, 0xff0000);
+}
+
+function update() {
+  // Update the circle in gameState.circle here
+ gameState.circle.y += 1;
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 600,
+	backgroundColor: "#99ff99",
+	scene: {
+    create,
+    update
+	}
+}
+
+const game = new Phaser.Game(config)
